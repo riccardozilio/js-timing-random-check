5f78520bb88c42fc7237cfc498b71194b2f35054
riccardozilio <riccardozilio.zilio@gmail.com>
2019-02-25 14:36:17 +0100

    correction1

---
+++ timing-random.js
@@ -0,0 +1,34 @@
+function getRandom(min, max) {
+
+  var delta = max - min + 1;
+  var rnd = Math.floor(Math.random() * (max - min)) + min;
+
+  return rnd;
+}
+function numarray() {
+  var nums = [];
+  var n;
+  for (var i = 0; i < 2; i++) {
+
+    n = getRandom(0, 100);
+    nums.push(n);
+    }
+  return nums;
+}
+alert(numarray());
+
+function numberImput(){
+var numeroConfronto = [];
+var numeroInserito;
+for (var i = 0; i < 2; i++) {
+  numeroInserito = prompt("dammi i numeri");
+  numeroConfronto.push(numeroInserito);
+}
+return numeroConfronto;
+}
+
+if (numarray() == numberImput()) {
+  alert("prossimo livello")
+}else {
+  alert("livello fallito")
+}
